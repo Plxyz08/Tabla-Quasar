fb6e3be695bbff1495e48ef6b5d7668b459b396d
Jeferson <sebastianmunoz603@gmail.com>
2023-10-10 17:51:50 -0500

    v11

---
+++ src/main.js
@@ -0,0 +1,23 @@
+import { createApp } from "vue";
+import "./style.css";
+import App from "./App.vue";
+import { router } from "./routes/routes.js";
+import { Quasar } from "quasar";
+import axios from 'axios';
+
+import "@quasar/extras/material-icons/material-icons.css";
+import "quasar/src/css/index.sass";
+
+const app = createApp(App);
+
+axios.defaults.baseURL="https://transporte-czaa.onrender.com/api/"
+
+app.use(Quasar, {
+    plugins: {},
+});
+
+app.use(router);
+
+app.mount("#app");
+
+
